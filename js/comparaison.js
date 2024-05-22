b20e026c3428b6629b9b068c2ab0cd77c65f7b05
Eathylik <louis.jcntpro@gmail.com>
2024-05-22 09:53:15 +0200

    js test

--- js/comparaison.js
+++ js/comparaison.js
@@ -1,6 +1,14 @@
-let a = 10;
+let a = "10";
 let b = 5 + 5;
 
-let c = a + b
+c = parseInt(a); // conversion en number
 
-console.log(a + b);
+console.log(a == b);// ceci est une expression (vrai ou fausse)
+console.log(a === b);
+console.log(a, typeof a);
+
+// le typeof permet de connaitre le type de la variable
+// il y a aussi le != que demande si A est different a B
+// le <= (inferieur ou égal) >= (supperieur ou egal) bien sur ils existent aussi seul (</>)
+//console. log(a === b); // le === test le type est la valeur
+//console. log(a == b); // le == test UNIQUEMENT la valeur => la chaine "10" est convertie en number
